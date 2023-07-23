4abbf56ec531ac066aeb542549429f6b8c6d0e35
Ahmed Meftah <contact@ahmedmeftah.com>
2023-07-23 22:43:48 +0200

    Update main program

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func goDotEnvVariable(key string) string {
@@ -20,87 +23,30 @@ func goDotEnvVariable(key string) string {
 	return os.Getenv(key)
 }
 
-type currentHabitInterface struct {
-	ID    string `json:"id"`
-	Title string `json:"title"`
-	MON   bool
-	TUE   bool
-	WED   bool
-	THU   bool
-	FRI   bool
-	SAT   bool
-	SUN   bool
-}
+var (
+	DB     *gorm.DB
+	server *gin.Engine
+)
 
-type JWTStructure struct {
-	Jwt string `json:"jwt"`
-}
+func init() {
+	password := goDotEnvVariable("DATABASE_PASSWORD")
+	username := goDotEnvVariable("DATABASE_USERNAME")
+	host := goDotEnvVariable("DATABASE_HOSTNAME")
+	port := goDotEnvVariable("DATABASE_HOSTPORT")
+	dbname := goDotEnvVariable("DATABASE_DATANAME")
 
-type UserStructure struct {
-	ID        string `json:"id"`
-	Avatar    string `json:"avatar"`
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Username  string `json:"username"`
-}
+	dsn := "host=" + host + " user=" + username + " password=" + password + " dbname=" + dbname + " port=" + port + " sslmode=disable"
 
-type loginResponse struct {
-	JWT  JWTStructure  `json:"jwt"`
-	User UserStructure `json:"user"`
-}
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
-var habits = []currentHabitInterface{
-	{ID: "4", Title: "teeth bruch every morning", MON: true, TUE: true, WED: true, THU: true, FRI: true, SAT: true, SUN: true},
-	{ID: "1", Title: "Drink water", MON: true, TUE: true, WED: true, THU: true, FRI: true, SAT: true, SUN: true},
-	{ID: "2", Title: "Read 10 pages from a book", MON: true, TUE: true, WED: true, THU: true, FRI: true, SAT: true, SUN: true},
-	{ID: "3", Title: "workout for 1h", MON: true, TUE: true, WED: true, THU: true, FRI: true, SAT: true, SUN: true},
-}
-
-type getCurrentHabitInterface struct {
-	Data []currentHabitInterface `json:"data"`
-	Week int                     `json:"week"`
-}
-
-func getCurrentHabits(c *gin.Context) {
-
-	var currentHabits = getCurrentHabitInterface{
-		Data: habits,
-		Week: 10,
+	if err != nil {
+		log.Fatal("Failed to connect to the Database")
 	}
+	fmt.Println("🚀 Connected Successfully to the Database")
 
-	c.IndentedJSON(http.StatusOK, currentHabits)
-}
+	server = gin.Default()
 
-func postAlbums(c *gin.Context) {
-	var newHabit currentHabitInterface
-
-	if err := c.BindJSON(&newHabit); err != nil {
-		return
-	}
-
-	habits = append(habits, newHabit)
-	c.IndentedJSON(http.StatusCreated, newHabit)
-}
-
-var user = loginResponse{
-	JWT: JWTStructure{
-		Jwt: "7nCqwyKJ9kh3jWvDng61UgBMXck8yhEme2jqyuceAPCxJ8Ts7jOR0Dy0UVpKuvDx9w7xP90BuKHYMsVWD18ZtcjgWh03IUfAORHyqf7ivZ",
-	},
-	User: UserStructure{
-		ID:        "f691ab0c-225d-11ee-be56-0242ac120002",
-		Avatar:    "/api/user/meftah/avatar",
-		FirstName: "Ahmed",
-		LastName:  "Meftah",
-		Username:  "meftah",
-	},
-}
-
-func login(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, user)
-}
-
-func getUserAvatar(c *gin.Context) {
-	c.File("./avatar/avatar.jpg")
 }
 
 func main() {
@@ -109,10 +55,11 @@ func main() {
 
 	gin.SetMode(mode)
 
-	router := gin.Default()
-	router.GET("/api/habit/current", getCurrentHabits)
-	router.POST("/api/auth/login", login)
-	router.GET("/api/auth", login)
-	router.GET("/api/user/:id/avatar", getUserAvatar)
-	router.Run("localhost:" + port)
+	router := server.Group("/api")
+	router.GET("/healthchecker", func(ctx *gin.Context) {
+		message := "Welcome to Two-Factor Authentication with Golang"
+		ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": message})
+	})
+
+	log.Fatal(server.Run("localhost:" + port))
 }
